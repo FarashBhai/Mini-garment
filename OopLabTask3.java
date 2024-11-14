cda5c0e8ebb2b0c2a126573a1ef06b52b84648ec
FarashBhai <purno23105101237@diu.edu.bd>
2024-11-14 19:55:50 +0600

    up9

--- OopLabTask3.java
+++ OopLabTask3.java
@@ -119,5 +119,27 @@ class Customer {
         System.out.println("Order Placed");
     }
 }
+class Inventory {
+
+    List<Garment> garments = new ArrayList<>();
+
+    void addGarment(Garment garment) {
+        garments.add(garment);
+    }
+
+    void removeGarment(String id) {
+        garments.removeIf(g -> g.id.equals(id)); 
+    }
+
+    Garment findGarment(String id) {
+        for (Garment g : garments) {
+            if (g.id.equals(id)) {
+                return g;
+            }
+        }
+        return null;
+    }
+}
+
 
 
